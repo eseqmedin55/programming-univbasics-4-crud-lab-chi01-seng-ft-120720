d68365b929a9559b96f90196051f02498da62801
esequiel medina <medinaesequiel@gmail.com>
2020-11-06 03:32:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
+  ["coder0", "coder1", "coder2", "coder3"]
+  end
   
-end
-
 def add_element_to_end_of_array(array, element)
-  
-end
+  array.push(element)
+  end
 
 def add_element_to_start_of_array(array, element)
-  
-end
+  array.unshift(element)
+  end
 
 def remove_element_from_end_of_array(array)
-  
-end
+  array.pop()
+  end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -31,7 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
